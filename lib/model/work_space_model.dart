5ca4550c26d943af67ab96080e11880b142d4c7d
iamudesharma <udesh2568@gmail.com>
2023-06-07 14:24:00 +0530

    board model and work space model added

---
+++ lib/model/work_space_model.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:trello_clone_appwrite/model/board_model.dart';
+
+class WorkSpaceModel {
+  final String ownerId;
+  final String name;
+  final List<String> members;
+  final List<BoardModel> board;
+  WorkSpaceModel({
+    required this.ownerId,
+    required this.name,
+    required this.members,
+    required this.board,
+  });
+
+  WorkSpaceModel copyWith({
+    String? id,
+    String? name,
+    List<String>? members,
+  }) {
+    return WorkSpaceModel(
+      ownerId: id ?? this.ownerId,
+      name: name ?? this.name,
+      members: members ?? this.members,
+      board: board ?? this.board,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': ownerId,
+      'name': name,
+      'members': List<dynamic>.from(members.map((x) => x)),
+    };
+  }
+
+  factory WorkSpaceModel.fromJson(Map<String, dynamic> json) => WorkSpaceModel(
+        ownerId: json["ownerId"],
+        name: json["name"],
+        members: List<String>.from(json["members"].map((x) => x)),
+        board: List<BoardModel>.from(
+            json["board"].map((x) => BoardModel.fromJson(x))),
+      );
+
+  String toJson() => json.encode(toMap());
+
+  @override
+  String toString() =>
+      'WorkSpaceModel(id: $ownerId, name: $name, members: $members)';
+
+  @override
+  bool operator ==(covariant WorkSpaceModel other) {
+    if (identical(this, other)) return true;
+
+    return other.ownerId == ownerId &&
+        other.name == name &&
+        listEquals(other.members, members);
+  }
+
+  @override
+  int get hashCode => ownerId.hashCode ^ name.hashCode ^ members.hashCode;
+}
