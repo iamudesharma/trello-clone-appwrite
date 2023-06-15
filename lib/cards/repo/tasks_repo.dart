ef08166a3886a358fd0294c844cf2dbbfbb78b38
iamudesharma <udesh2568@gmail.com>
2023-06-15 22:27:39 +0530

    task repo added

--- lib/cards/repo/tasks_repo.dart
+++ lib/cards/repo/tasks_repo.dart
@@ -7,3 +7,35 @@ abstract class ITaskRepo {
   Future deleteTask(String taskId);
   Future<void> updateTask(String taskId, Task taskModel);
 }
+
+class TaskRepo extends ITaskRepo {
+  @override
+  Future createTask(Task taskModel) {
+    // TODO: implement createTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future deleteTask(String taskId) {
+    // TODO: implement deleteTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future getTask(String taskId) {
+    // TODO: implement getTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Task>>? getTasks({required String id}) {
+    // TODO: implement getTasks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateTask(String taskId, Task taskModel) {
+    // TODO: implement updateTask
+    throw UnimplementedError();
+  }
+}
